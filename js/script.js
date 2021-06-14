62a3b27cd557b3e196a2537bc345b4fa3f681c1f
Mahmudul Hasan <85874828+mh-tomal@users.noreply.github.com>
2021-06-14 17:21:43 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,79 @@
+/*================================================
+| | | | Preloade
+================================================== */
+$(window).on('load', function () {
+    $('#status').fadeOut();
+    $('#preloader').delay(350).fadeOut('slow');
+});
+
+
+/*================================================
+| | | | place
+================================================== */
+
+$ (function() {
+    $("#img").owlCarousel({
+        item: 3,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause : true,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>']
+        
+        
+        
+    });
+})
+
+$(function(){
+    $('#gallery').magnificPopup({
+  delegate: 'a',
+  type: 'image',
+  gallery :{
+      enabled : true
+  }
+
+});
+});
+
+
+
+/* nav */
+
+$(function(){
+    
+    showHideNav();
+    
+    $(window).scroll(function(){
+        showHideNav();
+    });
+    
+    
+    function showHideNav(){
+        
+        if( $(window).scrollTop() >50){
+            $("nav").addClass("white-nav-top");
+            
+            $(".navbar-brand img").attr("src", "img/logo/logo-tb1.png");
+            
+        }
+        else{
+            
+            $("nav").removeClass("white-nav-top");
+            
+            $(".navbar-brand img").attr("src", "img/logo/logo.png");
+            
+            
+        }
+         
+    }
+    
+    
+});
+
+
+
+
+
