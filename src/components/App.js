2a861e303e3f7d113fcf557a385a2fd6bd36a0ab
Mahmudul Hasan <mahmudulhasan2020@gmail.com>
2022-01-03 19:15:47 +0600

    Quiz page complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import "../styles/App.css";
 import Layout from "./Layout";
+import Quiz from "./pages/Quiz";
 //import Home from "./pages/Home";
 //import Signup from "./pages/Signup";
-import Login from "./pages/Login";
+//import Login from "./pages/Login";
 
 function App() {
   return (
     <Layout>
-      <Login />
+      <Quiz />
     </Layout>
   );
 }
