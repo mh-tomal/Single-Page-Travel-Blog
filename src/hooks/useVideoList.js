8bb873adb3e7d0884269f29700a11bffabcb2640
Mahmudul Hasan <mahmudulhasan2020@gmail.com>
2022-01-10 02:41:32 +0600

    Not working

--- src/hooks/useVideoList.js
+++ src/hooks/useVideoList.js
@@ -1,10 +1,56 @@
-import { useEffect } from "react"
+import {
+  get,
+  getDatabase,
+  limitToFirst,
+  orderByKey,
+  query,
+  ref,
+  startAt,
+} from "firebase/database";
+import { useEffect, useState } from "react";
 
-export default function useVideolist(){
-    useEffect(()=>{
+export default function useVideolist(page) {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [videos, setVideos] = useState([]);
+  const [hasMore, setHasmore] = useState(true);
 
-    },[])
-    return(
+  useEffect(() => {
+    async function fetchVideos() {
+      const db = getDatabase();
+      const videosRef = ref(db, "videos");
+      const videoQuery = query(
+        videosRef,
+        orderByKey(),
+        startAt("" + page),
+        limitToFirst(8)
+      );
 
-    )
+      try {
+        setError(false);
+        setLoading(true);
+        const snapshot = await get(videoQuery);
+        setLoading(false);
+        if (snapshot.exists()) {
+          setVideos((prevVideos) => {
+            return [...prevVideos, ...Object.values(snapshot.val())];
+          });
+        } else {
+          setHasmore(false);
+        }
+      } catch (err) {
+        console.log(err);
+        setLoading(false);
+        setError(true);
+      }
+    }
+
+    fetchVideos();
+  }, [page]);
+  return {
+    loading,
+    error,
+    videos,
+    hasMore,
+  };
 }
